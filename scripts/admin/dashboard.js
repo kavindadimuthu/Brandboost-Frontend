e33b9b5003164c482aee7407d72d1858d1d468b7
Isuru N Liyanaarachchi <isurunaveen27@gmail.com>
2024-10-12 00:52:44 +0530

    complaints page created

--- scripts/admin/dashboard.js
+++ scripts/admin/dashboard.js
@@ -73,28 +73,46 @@ function loadUserTiles() {
 }
 
 
+
+
+
 // Sample array of complaints
 const complaints = [
     { user: 'Consequat', date: '02/08/2023', complaint: 'Deserunt minim incididunt cillum nostrudo voluptate excepteur.', status: 'Completed' },
     { user: 'Reprehende', date: '01/09/2023', complaint: 'Deserunt minim incididunt cillum nostrudo voluptate excepteur.', status: 'Completed' },
     { user: 'Labore', date: '15/12/2023', complaint: 'Deserunt minim incididunt cillum nostrudo voluptate excepteur.', status: 'Completed' },
     { user: 'Adipiscing', date: '12/10/2023', complaint: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.', status: 'Pending' },
-    { user: 'Velit', date: '05/06/2023', complaint: 'Aliquip consequat aute excepteur veniam nostrud excepteur labore.', status: 'In Progress' }
+    { user: 'Velit', date: '05/06/2023', complaint: 'Aliquip consequat aute excepteur veniam nostrud excepteur labore.', status: 'In-Progress' }
 ];
 
+
+function getStatusColor(status) {
+    switch (status) {
+        case 'Completed':
+            return '#27ae60';
+        case 'Pending':
+            return '#c0392b';
+        case 'In-Progress':
+            return '#2980b9';
+        default:
+            return 'black'; // Fallback color
+    }
+}
 
 function renderComplaints(complaints) {
     const complaintsContainer = document.querySelector('.customer-complaints tbody');
     complaintsContainer.innerHTML = ''; // Clear existing rows
 
     complaints.forEach(complaint => {
+        const statusColor = getStatusColor(complaint.status);
+
         // Create a new table row for each complaint
         const row = `
             <tr>
                 <td>${complaint.user}</td>
                 <td>${complaint.date}</td>
                 <td>${complaint.complaint}</td>
-                <td>${complaint.status}</td>
+                <td style="color: ${statusColor};    font-weight: 700;">${complaint.status}</td>
             </tr>
         `;
         complaintsContainer.innerHTML += row; // Append the row to the table
